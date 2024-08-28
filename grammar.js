1098ad2c7c19f7a7fb25d0986004a66dab8953df
Archer <66761259+ArcherHume@users.noreply.github.com>
2024-08-28 13:49:27 +1000

    Update grammar.js

--- grammar.js
+++ grammar.js
@@ -1,55 +1,151 @@
-// Note: if you ever need to explicitly match line breaks, those are system specific.
-// use a regex like this \r\n?|\n to match on windows too.
-
-function commaSep1(rule) {
-  return sep1(rule, ",");
-}
-
-function sep1(rule, separator) {
-  return seq(rule, repeat(seq(separator, rule)));
-}
-
 module.exports = grammar({
   name: "jinja2",
+
+  conflicts: ($) => [[$._expr, $.fn_call]],
 
   rules: {
     source_file: ($) =>
       repeat(
-        choice($._jinja_value, $.jinja_expression, $._jinja_comment, $._text),
+        choice(
+          $._jinja_value,
+          $.jinja_statement,
+          $._jinja_comment,
+          $.html_content,
+          $._text,
+        ),
       ),
 
     _jinja_value: ($) => seq("{{", $._expr, "}}"),
 
-    // This is awkward regex because we aren't parsing anything
-    // in between the expression markers like _jinja_value does
-    jinja_expression: ($) =>
+    jinja_statement: ($) =>
       seq(
         "{%",
-        new RegExp(
-          "(" + // capture group
-            "[^%]" + // any character that isn't a `%`
-            "|" + // or
-            "%[^}]" + // a `%` followed by any character that isn't `}`
-            ")*" + // zero or more of the previous capture group
-            "%}", // followed by a `%` then a `}`
+        optional("-"),
+        choice(
+          $.jinja_for,
+          $.jinja_if,
+          $.jinja_include,
+          $.jinja_extends,
+          $.jinja_block,
+          $.jinja_set,
+          $.jinja_macro,
+          $.jinja_call,
+          $.jinja_filter,
+          $.jinja_raw,
+          $.jinja_import,
+          $.jinja_from,
+          $.jinja_end_statement,
+        ),
+        optional("-"),
+        "%}",
+      ),
+
+    jinja_for: ($) =>
+      seq(
+        "for",
+        field("target", $._expr),
+        "in",
+        field("iterable", $._expr),
+        optional(field("if_clause", seq("if", $._expr))),
+      ),
+
+    jinja_if: ($) => seq("if", field("condition", $._expr)),
+
+    jinja_include: ($) =>
+      seq(
+        "include",
+        field("template", $.lit_string),
+        optional(seq("with", field("context", $.dict))),
+        optional("ignore missing"),
+      ),
+
+    jinja_extends: ($) =>
+      seq("extends", field("parent_template", $.lit_string)),
+
+    jinja_block: ($) => seq("block", field("block_name", $.identifier)),
+
+    jinja_set: ($) =>
+      seq("set", field("variable", $.identifier), "=", field("value", $._expr)),
+
+    jinja_macro: ($) =>
+      seq(
+        "macro",
+        field("macro_name", $.identifier),
+        field("params", $.argument_list),
+      ),
+
+    jinja_call: ($) =>
+      seq(
+        "call",
+        optional(field("macro", $._expr)),
+        optional(field("params", $.argument_list)),
+      ),
+
+    jinja_filter: ($) => seq("filter", field("filter_name", $.identifier)),
+
+    jinja_raw: ($) => "raw",
+
+    jinja_import: ($) =>
+      seq(
+        "import",
+        field("module", $.lit_string),
+        "as",
+        field("alias", $.identifier),
+      ),
+
+    jinja_from: ($) =>
+      seq(
+        "from",
+        field("module", $.lit_string),
+        "import",
+        commaSep1(
+          seq(
+            field("name", $.identifier),
+            optional(seq("as", field("alias", $.identifier))),
+          ),
         ),
       ),
 
-    // comment regex is special because a comment can end
-    // with #} ##} #######} etc.
+    jinja_end_statement: ($) =>
+      choice("endmacro", "endfor", "endif", "endblock", "endraw", "endcall"),
+
     _jinja_comment: ($) =>
+      seq("{#", new RegExp("(" + "(" + "[^#]" + "|" + "#[^}]" + ")*" + ")#+}")),
+
+    html_content: ($) =>
       seq(
-        "{#", // comments start with `{#`
-        new RegExp(
-          "(" + // capture group
-            "(" + // capture group
-            "[^#]" + // any character that isn't `#`
-            "|" + // or
-            "#[^}]" + // a `#` character followed by another character that isn't `}`
-            ")*" + // zero or more of the previous capture group
-            ")#+}", // followed by at least one `#` and a `}`
+        "<",
+        $.html_tag_name,
+        repeat($.html_attribute),
+        choice(
+          "/>",
+          seq(
+            ">",
+            repeat(
+              choice(
+                $.html_content,
+                $._text,
+                $._jinja_value,
+                $.jinja_statement,
+                $._jinja_comment,
+              ),
+            ),
+            "</",
+            $.html_tag_name,
+            ">",
+          ),
         ),
       ),
+
+    html_tag_name: ($) => /[a-zA-Z][a-zA-Z0-9-]*/,
+
+    html_attribute: ($) =>
+      seq(
+        $.html_attribute_name,
+        optional(seq("=", choice($.lit_string, $._jinja_value))),
+      ),
+
+    html_attribute_name: ($) => /[a-zA-Z][a-zA-Z0-9-]*/,
 
     _expr: ($) =>
       choice(
@@ -64,9 +160,12 @@ module.exports = grammar({
       ),
 
     fn_call: ($) =>
-      seq(
-        field("fn_name", $.identifier),
-        field("argument_list", $.argument_list),
+      prec(
+        1,
+        seq(
+          field("fn_name", $.identifier),
+          field("argument_list", $.argument_list),
+        ),
       ),
 
     argument_list: ($) =>
@@ -78,18 +177,7 @@ module.exports = grammar({
       ),
 
     lit_string: ($) =>
-      choice(
-        seq(
-          "'", // single quote string start
-          /([^']|\\')*/, // either not a `'` or a `\` followed by a `'` zero or more times
-          "'", // single quote string start
-        ),
-        seq(
-          '"', // double quote string start
-          /([^"]|\\")*/, // either not a `"` or a `\` followed by a `"` zero or more times
-          '"', // double quote string start
-        ),
-      ),
+      choice(seq("'", /([^']|\\')*/, "'"), seq('"', /([^"]|\\")*/, '"')),
 
     bool: ($) => choice("True", "False"),
 
@@ -101,28 +189,12 @@ module.exports = grammar({
 
     identifier: ($) => $._identifier,
 
-    // This regex is fine until we allow user-named variables and functions.
-    // Once we do that we may want to allow Unicode identifiers like python does: /[_\p{XID_Start}][_\p{XID_Continue}]*/
-    _identifier: ($) =>
-      token(
-        new RegExp(
-          "[a-zA-Z_]" + // starts with a lower or upper letter or an underscore
-            "[a-zA-Z0-9_]*", // all following characters must be a lower or upper letter, digit, or underscore.
-        ),
-      ),
+    _identifier: ($) => token(new RegExp("[a-zA-Z_]" + "[a-zA-Z0-9_]*")),
 
     kwarg: ($) => seq(field("key", $.identifier), "=", field("value", $._expr)),
 
-    // matches everything but jinja
     _text: ($) =>
-      new RegExp(
-        "(" + // capture group
-          "[^{]" + // match any character that is not `{`
-          "|" + // or
-          "[{][^{%#]" + // match a character that IS `{` and isn't followed by `{`, `%`, or`#`
-          ")" + // end capture group
-          "+", // one or more times. using this instead of * because tree-sitter can hang when matching the empty string.
-      ),
+      token(prec(-1, new RegExp("(" + "[^<{]" + "|" + "[{][^{%#]" + ")+"))),
 
     integer: ($) => token(seq(optional(/[\+-]/), repeat1(/_?[0-9]+/))),
 
@@ -153,3 +225,11 @@ module.exports = grammar({
     },
   },
 });
+
+function commaSep1(rule) {
+  return sep1(rule, ",");
+}
+
+function sep1(rule, separator) {
+  return seq(rule, repeat(seq(separator, rule)));
+}
